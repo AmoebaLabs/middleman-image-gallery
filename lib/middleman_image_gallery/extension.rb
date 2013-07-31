f4da7d4601370f6f5837015729cfee4c9ed7a766
Steve Gehrman <sgehrman@gmail.com>
2013-07-31 16:37:08 -0700

    updated version

--- lib/middleman_image_gallery/extension.rb
+++ lib/middleman_image_gallery/extension.rb
@@ -1,16 +1,18 @@
 require "middleman-core"
 
-class Middleman::ThumbnailGenerator < Middleman::Extension
-  def initialize(app, options_hash={}, &block)
-    super
+module Middleman
 
-    app.after_build do |builder|
-      puts '#### yeah!! ####'
+  class ThumbnailGenerator < Extension
+    def initialize(app, options_hash={}, &block)
+      super
+
+      app.after_build do |builder|
+        puts '#### yeah!! ####'
+      end
+
     end
-
   end
 
-  alias :included :registered
 end
 
 ::Middleman::Extensions.register(:image_gallery, Middleman::ThumbnailGenerator)
