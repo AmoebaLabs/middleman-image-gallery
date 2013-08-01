923c2058c2c902f2bb12e8a800e1168874452895
Steve Gehrman <sgehrman@gmail.com>
2013-07-31 17:17:40 -0700

    updated version

--- lib/middleman_image_gallery/extension.rb
+++ lib/middleman_image_gallery/extension.rb
@@ -6,9 +6,33 @@ module Middleman
     def initialize(app, options_hash={}, &block)
       super
 
+      #app.after_build do |builder|
+      #  puts '#### yeah!! ####'
+      #end
+      #
+
+      dimensions = options[:dimensions]
+      namespace = options[:namespace_directory].join(',')
+
+      dir = Pathname.new(File.join(source_dir, images_dir))
+
       app.after_build do |builder|
-        puts '#### yeah!! ####'
+
+        puts 'building...'
+
+        file_types = [:jpg, :jpeg, :png]
+
+        glob = "#{dir}/#{namespace}/*.{#{file_types.join(',')}}"
+        files = Dir[glob]
+
+        files.each do |file|
+          path = file.gsub(source_dir, '')
+
+          puts path
+
+        end
       end
+
 
     end
   end
