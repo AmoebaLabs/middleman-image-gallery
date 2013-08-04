138c68ab3ec9a7f35942e220fc614ce531ca2431
Steve Gehrman <sgehrman@gmail.com>
2013-08-04 00:16:53 -0700

    refactor

--- lib/middleman_image_gallery/extension.rb
+++ lib/middleman_image_gallery/extension.rb
@@ -1,5 +1,6 @@
 require "middleman-core"
 require 'middleman_image_gallery/thumbnail_generator'
+require 'middleman_image_gallery/helpers'
 
 module Middleman
 
@@ -7,140 +8,24 @@ module Middleman
     def initialize(app, options_hash={}, &block)
       super
 
-      # append helper methods
-      app.helpers Middleman::HelperMethods
-
       # after build callback
       app.after_build do |builder|
-        ThumbnailExtension.createThumbnails(self)
-      end
-
-      # after configuration callback
-      app.after_configuration do
+        writer = ThumbnailWriter.new(self)
+        writer.write
       end
     end
 
-    private
-
-    def self.createThumbnails(app)
-      folders = HelperMethods.gallery_folder_names(app, true)
-
-      folders.each do |galleryFolder|
-        # create thumbnails directory to write thumbnails
-        thumbsDirectory = Pathname.new(File.join(galleryFolder, "thumbnails"))
-        thumbsDirectory.mkpath() if not thumbsDirectory.exist?
-
-        images = HelperMethods.images_for_gallery(app, galleryFolder.basename().to_s, true)
-        images.each do |file|
-          dimensions = { small: '200x' }
-
-          specs = ThumbnailGenerator.specs(file.basename().to_s, dimensions)
-          ThumbnailGenerator.generate(galleryFolder.to_s, thumbsDirectory.to_s, file.basename().to_s, specs)
-        end
+    # exposed helper to create a gallery
+    # put this in your erb file
+    # <%= gallery_div(data.gallery.gallery-name, "section-name") %>
+    helpers do
+      def gallery_div(data, name, opts={})
+        gallery = GalleryGenerator.new(self)
+        gallery.gen(data, name, opts)
       end
-    end
-  end
-
-  module HelperMethods
-    def self.gallery_folder_names(app, build_mode=false)
-      result = []
-      dir = HelperMethods.gallery_dir(app, build_mode)
-
-      dir.children.each do |item|
-        if item.directory?
-          result << item
-        end
-      end
-
-      result
-    end
-
-
-    def self.gallery_dir(app, build_mode=false)
-      if build_mode
-        result = File.join(Application.root, app.build_dir, app.settings.images_dir)
-      else
-        result = File.join(app.source_dir, app.settings.images_dir)
-      end
-
-      result = File.join(result, "gallery")
-
-      Pathname.new(result)
-    end
-
-    def self.images_for_gallery(app, name, build_mode=false)
-      result = []
-      dir = Pathname.new(File.join(HelperMethods.gallery_dir(app, build_mode), name))
-
-      dir.children.each do |file|
-        if file.file? && HelperMethods.gallery_image?(file.basename.to_s)
-          result << file
-        end
-      end
-
-      result
-    end
-
-    def self.gallery_image?(name)
-      !(name =~ /\.(?:jpe?g|png|gif)$/i).nil?
-    end
-
-    def gallery_images(data, name, opts={})
-      files = HelperMethods.images_for_gallery(self, name)
-
-      itemsContent = ''
-      files.each do |file|
-        base = file.basename('.*')
-
-        caption = 'unknown'
-        obj = data[name][base]
-        caption = obj.caption if obj.present?
-
-        itemsContent += make_image_tag(file, caption)
-      end
-
-      # gallery wrapping div
-      content = content_tag(:div, itemsContent, class: 'gallery')
-
-      content
-    end
-
-    private
-
-    def make_image_tag(file, caption)
-      path = file.to_s.gsub(source_dir, '')
-
-      # get thumbnail if it exists
-
-      thumbnameFile = Pathname.new(File.join(file.parent, 'thumbnails', file.basename))
-      thumbnailPath = ''
-      thumbnailPath = thumbnameFile.to_s.gsub(source_dir, '') if thumbnameFile.exist?
-
-      thumbnailPath = path if thumbnailPath.blank?
-
-      puts "file: #{thumbnameFile}"
-      puts "path: #{thumbnailPath}"
-
-
-      # create image
-      content = image_tag(thumbnailPath, alt: "a picture")
-
-      # append a p tag for the caption
-      caption = path if caption.blank?
-
-      content << content_tag(:p, caption)
-
-      # wrap link around image
-      content = content_tag(:a, content, href: path)
-
-      # wrap content up in a final wrapper div
-      content = content_tag(:div, content, class: 'gallery-item')
-
-      content
     end
 
   end
-
 end
 
 ::Middleman::Extensions.register(:image_gallery, Middleman::ThumbnailExtension)
---
+++ lib/middleman_image_gallery/helpers.rb
@@ -0,0 +1,157 @@
+module Middleman
+
+  # =========================================================
+  # =========================================================
+
+  class ThumbnailWriter
+    def initialize(app)
+      @app = app
+      @gallery = GalleryDirectoryHelper.new(app)
+    end
+
+    def write
+      createThumbnails
+    end
+
+    private
+
+    def createThumbnails()
+      folders = @gallery.directories(true)
+
+      folders.each do |galleryFolder|
+        # create thumbnails directory to write thumbnails
+        thumbsDirectory = Pathname.new(File.join(galleryFolder, "thumbnails"))
+        thumbsDirectory.mkpath() if not thumbsDirectory.exist?
+
+        images = @gallery.images(galleryFolder.basename().to_s, true)
+        images.each do |file|
+          dimensions = { small: '200x' }
+
+          specs = ThumbnailGenerator.specs(file.basename().to_s, dimensions)
+          ThumbnailGenerator.generate(galleryFolder.to_s, thumbsDirectory.to_s, file.basename().to_s, specs)
+        end
+      end
+    end
+
+  end
+
+  # =========================================================
+  # =========================================================
+
+  class GalleryGenerator
+    def initialize(app)
+      @app = app
+      @gallery = GalleryDirectoryHelper.new(app)
+    end
+
+    def gen(data, name, opts)
+      files = @gallery.images(name)
+
+      itemsContent = ''
+      files.each do |file|
+        base = file.basename('.*').to_s
+
+        caption = 'unknown'
+        obj = data[name][base]
+        caption = obj.caption if obj.present?
+
+        itemsContent += make_image_tag(file, caption)
+      end
+
+      # gallery wrapping div
+      content = @app.content_tag(:div, itemsContent, class: 'gallery')
+
+      content
+    end
+
+    private
+
+    def make_image_tag(file, caption)
+      path = file.to_s.gsub(@app.source_dir, '')
+
+      # get thumbnail if it exists
+
+      thumbnameFile = Pathname.new(File.join(file.parent, 'thumbnails', file.basename))
+      thumbnailPath = ''
+      thumbnailPath = thumbnameFile.to_s.gsub(@app.source_dir, '') if thumbnameFile.exist?
+
+      thumbnailPath = path if thumbnailPath.blank?
+
+      # create image
+      content = @app.image_tag(thumbnailPath, alt: "a picture")
+
+      # append a p tag for the caption
+      caption = path if caption.blank?
+
+      content << @app.content_tag(:p, caption)
+
+      # wrap link around image
+      content = @app.content_tag(:a, content, href: path)
+
+      # wrap content up in a final wrapper div
+      content = @app.content_tag(:div, content, class: 'gallery-item')
+
+      content
+    end
+
+  end
+
+  # =========================================================
+  # =========================================================
+
+  class GalleryDirectoryHelper
+    def initialize(app)
+      @app = app
+    end
+
+    def directories(build_mode=false)
+      result = []
+      dir = directory(build_mode)
+
+      dir.children.each do |item|
+        if item.directory?
+          result << item
+        end
+      end
+
+      result
+    end
+
+    def directory(build_mode=false)
+      if build_mode
+        result = File.join(Application.root, @app.build_dir, @app.settings.images_dir)
+      else
+        result = File.join(@app.source_dir, @app.settings.images_dir)
+      end
+
+      result = File.join(result, "gallery")
+
+      Pathname.new(result)
+    end
+
+    def images(name, build_mode=false)
+      result = []
+      dir = Pathname.new(File.join(directory(build_mode), name))
+
+      dir.children.each do |file|
+        if file.file? && gallery_image?(file.basename.to_s)
+          result << file
+        end
+      end
+
+      result
+    end
+
+    # ============
+    # Private
+    # ============
+
+    def gallery_image?(name)
+      !(name =~ /\.(?:jpe?g|png|gif)$/i).nil?
+    end
+
+  end
+
+end
+
+
